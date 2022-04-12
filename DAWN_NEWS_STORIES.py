7b7a8b6e0b0db1cb60ea9944c59f9cafc2e61a66
Mohammed faheem <44580643+faheem77@users.noreply.github.com>
2022-04-12 21:00:55 +0500

    Update DAWN_NEWS_STORIES.py

--- DAWN_NEWS_STORIES.py
+++ DAWN_NEWS_STORIES.py
@@ -2,9 +2,7 @@ import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import time
 import pandas as pd
-from tqdm import tqdm
 from multiprocessing import Process
-import os
 def urls():
   links=[]
   session= requests.session()
@@ -13,7 +11,6 @@ def urls():
     url= f'https://www.dawn.com/trends/no-confidence/{i}'
     only_a_tags= SoupStrainer('a')
     r= session.get(url,headers=headers)
-    # print(r.status_code)
     soup= BeautifulSoup(r.content,'lxml', parse_only=only_a_tags)
     for x in soup.findAll('a', class_='story__link'):
       links.append(x.get('href'))
@@ -28,8 +25,6 @@ def scrap():
   for url in urls():
     headers= {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}
     r= session.get(url,headers=headers)
-    print("current process: ", os.getpid())
-    print(r.status_code)
     tags= SoupStrainer(['article','div','a','span'])
     soup=BeautifulSoup(r.content,'lxml', parse_only=tags)
     for i in soup.findAll('article', class_=['story','single','bg-white font-georgia']):
